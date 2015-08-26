f078479ccbf2599735d1c771b21d13a295fb6a1f
sahilbadyal <sbadyals@gmail.com>
2015-08-27 02:35:20 +0530

    Added DATABASE URL

--- inno_eco_website/settings.py
+++ inno_eco_website/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -63,10 +64,7 @@ WSGI_APPLICATION = 'inno_eco_website.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 # Internationalization
@@ -84,7 +82,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# 
+#
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
@@ -98,11 +96,3 @@ EMAIL_HOST = 'smtp.mandrillapp.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'innovationecosystem.nith@gmail.com'
 EMAIL_HOST_PASSWORD = 'F5NXAYT0tDMq7JkRtiU-2g'
-
-
-
-
-
-
-
-
